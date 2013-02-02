976ac546b4f559698d447cb35433f62b7c2f4187
Jonathan <js2407@txstate.edu>
2013-02-01 22:59:34 -0600

    4th comment

--- Hearts/src/edu/txstate/hearts/model/Achievements.java
+++ Hearts/src/edu/txstate/hearts/model/Achievements.java
@@ -10,7 +10,8 @@ package edu.txstate.hearts.model;
 
 // Comment to commit --Jonathan
 // Comment for 2nd commit --Jonathan (there should be one comment above this)
-// 3rd comment line
+// 3rd comment line (laptop)
+// 4th comment line (desktop)
 public class Achievements {
 
 }
