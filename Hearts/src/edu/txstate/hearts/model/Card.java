462416816772d7fc2faccb026cc9161b93921dd9
sutapaig <sutapaig@yahoo.com>
2013-02-01 20:15:53 -0600

    testing

--- Hearts/src/edu/txstate/hearts/model/Card.java
+++ Hearts/src/edu/txstate/hearts/model/Card.java
@@ -9,8 +9,38 @@ package edu.txstate.hearts.model;
  */
 public class Card {
 
-	private String suit;
-	private String face;
+	public static enum Face { Ace, Deuce, Three, Four, Five, Six
+		, Seven, Eight, Nine, Ten, Jack, Queen, King};
+		
+	public static enum Suit { Clubs, Diamonds, Hearts, Spades};
+	
+	private final Face face;
+	private final Suit suit;
 	private int pointValue;
-
+	
+	public Card(Face cardFace, Suit cardSuit)
+	{
+		this.face = cardFace;
+		this.suit = cardSuit;
+	}
+	
+	public Face getFace()
+	{
+		return this.face;
+	}
+	
+	public Suit getSuit()
+	{
+		return this.suit;
+	}
+	
+	public int getPointValue()
+	{
+		return this.pointValue;
+	}
+	
+	public String toString()
+	{
+		return String.format("%s of %s", face, suit);
+	}
 }
