5b48a6d7c0c295b5dba3a0a4f39be434bf923616
Jonathan <js2407@txstate.edu>
2013-02-01 22:35:58 -0600

    2nd commit for achievements

--- Hearts/src/edu/txstate/hearts/model/Achievements.java
+++ Hearts/src/edu/txstate/hearts/model/Achievements.java
@@ -9,6 +9,7 @@ package edu.txstate.hearts.model;
  */
 
 // Comment to commit --Jonathan
+// Comment for 2nd commit --Jonathan (there should be one comment above this)
 public class Achievements {
 
 }
