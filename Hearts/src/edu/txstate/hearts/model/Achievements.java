9793997fde6cda9775b6eca4bdd7a43f5a6f8426
jdsaustin <js2407@txstate.edu>
2013-02-05 20:10:02 -0600

    Remove test comments

--- Hearts/src/edu/txstate/hearts/model/Achievements.java
+++ Hearts/src/edu/txstate/hearts/model/Achievements.java
@@ -8,12 +8,7 @@ package edu.txstate.hearts.model;
  *
  */
 
-// Comment to commit --Jonathan
-// Comment for 2nd commit --Jonathan (there should be one comment above this)
-// 3rd comment line (laptop)
-// 4th comment line (desktop)
-// 5th comment (laptop)
-// 6th comment (desktop)
+
 public class Achievements {
 
 }
