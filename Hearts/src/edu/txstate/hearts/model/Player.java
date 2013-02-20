5dbb16d832ddba67043ba5b74e0eba64d8eb0cdc
nstickels <nstickels@gmail.com>
2013-02-19 20:58:33 -0600

    Trying again to add the new logic for determining how many ways x number of cards can be divided between the other three players.

--- Hearts/src/edu/txstate/hearts/model/Player.java
+++ Hearts/src/edu/txstate/hearts/model/Player.java
@@ -1,18 +1,23 @@
 /**
- * 
+ *
  */
 package edu.txstate.hearts.model;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
+import java.util.Set;
+
+//import org.apache.commons.math3.util.ArithmeticUtils;
+
 import edu.txstate.hearts.model.Card.Suit;
 
 /**
  * @author Neil Stickels, I Gede Sutapa
- * 
+ *
  */
 public abstract class Player {
 
@@ -21,13 +26,13 @@ public abstract class Player {
 	private List<Card> hand;
 	private Random rand = new Random();
 	private List<Card> inPlayCards; //keep track cards that are being played on the table
-	private List<Card> playedCards; //keep track cards that have been played in the past
+	private Set<Card> playedCards; //keep track cards that have been played in the past
 	private List<Card> takenCards; //keep track cards that the player took
-	
+
 	public Player(String playerName) {
 		hand = new ArrayList<Card>();
 		inPlayCards = new ArrayList<Card>();
-		playedCards = new ArrayList<Card>();
+		playedCards = new HashSet<Card>();
 		takenCards = new ArrayList<Card>();
 		this.name = playerName;
 	}
@@ -53,7 +58,7 @@ public abstract class Player {
 		for(int i = 0; i < cards.size(); i++)
 			addCard(cards.get(i));
 	}
-	
+
 	public void printHand() {
 		for (int i = 0; i < hand.size(); i++) {
 			System.out.printf("%-19s%s", hand.get(i), ((i + 1) % 4 == 0) ? "\n"
@@ -76,15 +81,15 @@ public abstract class Player {
 			boolean veryFirstTurn);
 
 	public abstract List<Card> getCardsToPass();
-	
+
 	protected List<Card> getHand() {
 		return hand;
 	}
-	
+
 	protected List<Card> getLegalCards(List<Card> cardsPlayed, boolean heartsBroken)
 	{
 		List<Card> playable = new ArrayList<Card>();
-		
+
 		//if I am not the first player
 		if(cardsPlayed.size() > 0)
 		{
@@ -96,7 +101,7 @@ public abstract class Player {
 				if(c.getSuit() == ledCard.getSuit())
 				{
 					playable.add(c);
-				} 
+				}
 				else
 				{
 					otherSuits.add(c);
@@ -105,7 +110,7 @@ public abstract class Player {
 			//if I cannot find anything within the same suit that the first player did
 			if(playable.size() == 0)
 				playable.addAll(otherSuits);
-		} 
+		}
 		else
 		{
 			//if hearts is already broken, player can use any cards
@@ -122,7 +127,7 @@ public abstract class Player {
 						break;
 					}
 				}
-				
+
 				//but all I have all hearts
 				if(allHearts)
 					playable.addAll(getHand());
@@ -138,7 +143,7 @@ public abstract class Player {
 		}
 		return playable;
 	}
-	
+
 	protected Random getRand()
 	{
 		return rand;
@@ -150,7 +155,7 @@ public abstract class Player {
 		//also add to the played cards
 		addPlayedCards(card);
 	}
-	
+
 	public void clearInPlayCards()
 	{
 		this.inPlayCards.clear();
@@ -166,7 +171,7 @@ public abstract class Player {
 	{
 		this.playedCards.add(card);
 	}
-	
+
 	public void clearPlayedCards()
 	{
 		this.playedCards.clear();
@@ -177,11 +182,12 @@ public abstract class Player {
 		this.takenCards.add(card);
 		System.out.println("Add " + card.toString() + " to " + this.getName());
 	}
-	
+
 	public void clearTakenCards()
 	{
 		this.takenCards.clear();
 	}
+
 
 	public List<Card> getTakenCards()
 	{
@@ -199,5 +205,51 @@ public abstract class Player {
 		clearPlayedCards();
 		clearTakenCards();
 	}
+
+	public long getTotalCardCombinations(Card.Suit suit, int myCount)
+	{
+		int available = 13-myCount;
+		Iterator<Card> iterator = playedCards.iterator();
+		while(iterator.hasNext())
+		{
+			Card c = iterator.next();
+			if(c.getSuit() == suit)
+				available--;
+		}
+		System.out.println("available is "+available);
+		return (long)(Math.pow(3, available));
+//		long totalCombos = 0;
+//		for(int x = 0; x < available+1; x++)
+//		{
+//			for(int y = 0; y < available+1; y++)
+//			{
+//				for(int z= 0; z < available+1; z++)
+//				{
+//					if(x+y+z == available)
+//					{
+//					long xNewCombos, yNewCombos, zNewCombos;
+//					xNewCombos = ArithmeticUtils.binomialCoefficient(available, x);
+//					//System.out.println("adding "+available+"C"+x+" for player 1 - "+xNewCombos);
+//					//totalCombos += newCombos;
+//					yNewCombos = ArithmeticUtils.binomialCoefficient(available-x, y);
+//					//System.out.println("adding "+(available-x)+"C"+y+" for player 2 - "+yNewCombos);
+//					//totalCombos += newCombos;
+//					zNewCombos = ArithmeticUtils.binomialCoefficient(available-x-y, z);
+//					//System.out.println("adding "+(available-x-y)+"C"+z+" for player 3 - "+zNewCombos);
+//					long newCombos = (xNewCombos*yNewCombos*zNewCombos);
+//					if(newCombos < 1)
+//						newCombos = 1;
+//					totalCombos += newCombos;
+//					//System.out.println("adding "+newCombos);
+//
+//					//System.out.println("---");
+//
+//					}
+//				}
+//			}
+//		}
+//		return totalCombos;
+	}
+
 }
 
