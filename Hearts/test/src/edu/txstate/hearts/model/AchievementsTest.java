451f3450c641ffb63c3c00b2d85c152e509c1a28
Jonathan <js2407@txstate.edu>
2013-04-03 21:52:19 -0500

    Changes to achievements test

--- Hearts/test/src/edu/txstate/hearts/model/AchievementsTest.java
+++ Hearts/test/src/edu/txstate/hearts/model/AchievementsTest.java
@@ -15,22 +15,22 @@ import org.junit.Test;
 //import edu.txstate.hearts.model.Achievements.Achievement;
 
 /**
- * @author Jonathan
+ * @author Jonathan Shelton
  *
  */
 public class AchievementsTest {
 
-	private Player p;
-	private final static String playerName = "TestPlayer";
+	private User p;
+	private String playerName = "initialize";
 	private Deck deck;
-	private Achievements a = new Achievements();
+	private Achievements a = new Achievements(playerName);
 	
 	/**
 	 * @throws java.lang.Exception
 	 */
 	@Before
 	public void setUp() throws Exception {
-		p = new User(playerName, 0);
+		p = new User("TestPlayer", 0);
 		deck = new Deck();
 		deck.shuffleCards();
 	}
@@ -47,6 +47,8 @@ public class AchievementsTest {
 	 */
 	@Test
 	public void testAchievements() {
+		
+		//Ensure all achievements have been placed in list
 		assertEquals("Map doesn't have BrokenHeart achievement",true,a.listOfAchievements.containsKey
 				("BrokenHeart"));
 		assertEquals("Map doesn't have HatTrick achievement",true,a.listOfAchievements.containsKey
@@ -63,6 +65,10 @@ public class AchievementsTest {
 				("ShootingTheMoon"));
 		assertEquals("Map doesn't have StartTheParty achievement",true,a.listOfAchievements.containsKey
 				("StartTheParty"));
+		
+		//Ensure that player name is being passed correctly
+		String checkName = "TestPlayer";
+		assertEquals("Player Name isn't correct,", checkName, p.getAchievements().getUserFileName());
 	}
 
 	/**
@@ -70,7 +76,7 @@ public class AchievementsTest {
 	 */
 	@Test
 	public void testGetCounterOvershootingTheMoon() {
-		assertEquals("Counter is not 0",a.getCounterOvershootingTheMoon(),0);
+		assertEquals("Counter is not 0",0,a.getCounterOvershootingTheMoon());
 	}
 
 	/**
@@ -78,7 +84,9 @@ public class AchievementsTest {
 	 */
 	@Test
 	public void testSetCounterOvershootingTheMoon() {
-		fail("Not yet implemented");
+		a.setCounterOvershootingTheMoon(2);
+		assertEquals("Counter not set correctly,",2,a.getCounterOvershootingTheMoon());
+		
 	}
 
 	/**
@@ -102,22 +110,25 @@ public class AchievementsTest {
 		//assertEquals("Achievement not set,",true,a.achievedOrNot);
 		//assertEquals("Achievement notification not working,",true,a.notifyAchievementEarned);
 		
-		//Pass in a score of 26. Test "Overshooting The Moon1" achievement
+		//Pass in first score of 26. Test "Overshooting The Moon1" & Shooting The M0on
 		a.endGameAchievement(26, false);
+		assertEquals("ShootingTheMoon hasn't been set,",true,a.listOfAchievements.get
+				("ShootingTheMoon"));
 		assertEquals("OvershootingTheMoon1 hasn't been set,",true,a.listOfAchievements.get
 				("OvershootingTheMoon1"));
+		//assertEquals("Counter is not 0,",0,a.getCounterOvershootingTheMoon());
+		assertEquals("Counter is not 1,",1,a.getCounterOvershootingTheMoon());
 		
 		//Pass in a second score of 26. Test "Overshooting The Moon2" achievement
 		a.endGameAchievement(26, false);
-		assertEquals("Counter is not 1,",a.getCounterOvershootingTheMoon(),1);
 		assertEquals("OvershootingTheMoon2 hasn't been set,",true,a.listOfAchievements.get
 				("OvershootingTheMoon2"));
 		assertEquals("OvershootingTheMoon3 has been set,",false,a.listOfAchievements.get
 				("OvershootingTheMoon3"));
+		assertEquals("Counter is not 2,",2,a.getCounterOvershootingTheMoon());
 		
 		//Pass in a third score of 26. Test "Overshooting The Moon3" achievement
 		a.endGameAchievement(26, false);
-		assertEquals("Counter is not 2,",a.getCounterOvershootingTheMoon(),2);
 		assertEquals("OvershootingTheMoon2 hasn't been set,",true,a.listOfAchievements.get
 				("OvershootingTheMoon3"));
 		
