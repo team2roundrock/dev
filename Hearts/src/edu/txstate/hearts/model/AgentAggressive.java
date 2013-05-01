f07bfed017b8b983260755daea2d7a4dd80694b8
nstickels <nstickels@gmail.com>
2013-04-30 19:00:54 -0500

    Added full javadoc comments to AgentAggressive

--- Hearts/src/edu/txstate/hearts/model/AgentAggressive.java
+++ Hearts/src/edu/txstate/hearts/model/AgentAggressive.java
@@ -30,6 +30,25 @@ import edu.txstate.hearts.utils.RiskThresholds.Threshold;
 /**
  * Contains rules & strategies for the most difficult AI agent
  * 
+ * The basic strategy of the AgentAggressive is to play the Queen of Spades
+ * or a Heart on a user if possible, otherwise determine the best card to play
+ * by finding the highest card in the smallest suit that is under the current
+ * risk threshold.  This is done leveraging the RiskThreshold class, which
+ * contains multiple Thresholds for risk under various situations, which each
+ * get adjusted based on the result of what happened each hand.
+ * 
+ * At a simple level, the risk being compared to the risk threshold is a
+ * calculation of the probability that one of the players yet to play does not
+ * have any cards in the suit led and that this player also has hearts, and 
+ * the probability that if this card is played, that this card would win the
+ * hand.  If this value exceeds the risk threshold, it is determined that this
+ * card is too risky to play.  If it is under the risk threshold, the card is
+ * fine to play.
+ * 
+ * After each turn is done, look at the cards played in that turn.  If my card
+ * won the trick, determine if I took any points, and if so, decrease the risk
+ * threshold.  If my card did not win the trick, increase the risk threshold.
+ * 
  * @author Neil Stickels, Jonathan Shelton
  * 
  */
@@ -43,6 +62,13 @@ public class AgentAggressive extends Agent {
 	private Threshold lastThreshold;
 	private final static boolean silent = Hearts.silent;
 
+	/**
+	 * Contruct an Aggressive Agent with the name specified and as the player
+	 * number specified
+	 * @param playerName the name to display for this player as a String
+	 * @param num the index of the player in the list of players for the game
+	 * @see edu.txstate.hearts.model.Agent#Agent(String, int)
+	 */
 	public AgentAggressive(String playerName, int num) {
 		super(playerName, num);
 		try {
@@ -58,16 +84,17 @@ public class AgentAggressive extends Agent {
 				input.close();
 			}
 		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
 			System.out.println("problem reading in file thresholds.bin: "
 					+ e.getMessage());
 			thresholds = new RiskThresholds();
 		}
-		//System.out.println("thresholds is "+thresholds);
 	}
 
+	/**
+	 * Save all of the current threshold objects to disk 
+	 */
 	public void serializeThresholds() {
 		try {
 			// use buffering
@@ -86,9 +113,29 @@ public class AgentAggressive extends Agent {
 		}
 	}
 
-	/*
-	 * (non-Javadoc)
+	/**
+	 * Figure out the cards to pass.  The Aggressive Agent's logic for passing
+	 * cards:<br>
+	 * While the number of cards to pass is less than 3:
+	 * <li>determine the suit with the fewest cards
+	 * <li>determine the number of cards within that suit
+	 * <li>if the suit with the fewest cards is clubs or diamonds and the 
+	 * number of cards in that suit contains the number of cards to pass or 
+	 * fewer cards, pass all of those cards
+	 * <li>if the suit with the fewest cards is spades, and you only have one
+	 * spade, the queen of spades, pass the queen of spades
+	 * <li>if the number of cards in the suit with the fewest is greater than
+	 * the number of cards to pass, if you have the two of clubs, pass that
+	 * <li>if the suit with the fewest cards is diamonds or clubs and you have
+	 * more than the number of cards to pass in that suit, pass the highest
+	 * cards you have in that suit up to the number of cards to pass
+	 * <li>if the suit with the fewest cards is spades, and you have more than 
+	 * 4 spades and you have the Ace, King or Queen of spades pass those
+	 * <li>if you get here and still have cards to pass, pass your highest
+	 * hearts
 	 * 
+	 * @param passing enumeration for which way to pass the cards
+	 * @return the List of Cards being passed
 	 * @see edu.txstate.hearts.model.Agent#getCardsToPass(Passing)
 	 */
 	@Override
@@ -249,6 +296,8 @@ public class AgentAggressive extends Agent {
 					fewest = getCardsFromSuitWithFewest(Collections.<Card> emptyList(), diamonds, clubs);
 				} else
 				{
+					// if we got here, this means something bad happened, as
+					// this should never happen
 					System.out.println("I am here");
 					try
 					{
@@ -262,7 +311,8 @@ public class AgentAggressive extends Agent {
 		}
 		if(cardsToPass.size() > 3)
 		{
-			//wtf?
+			// this should never happen, just here to catch any error in case
+			// we are passing more than 3 cards
 			System.out.println("oh nos");
 			System.out.println("passing "+cardsToPass);
 			System.out.println("hand is "+myHand);
@@ -298,9 +348,42 @@ public class AgentAggressive extends Agent {
 		return cardsToPass;
 	}
 
-	/*
-	 * (non-Javadoc)
+	/**
+	 * The bulk of the Aggressive Agent's logic is in this method, which will
+	 * decide the best card to play each turn.
+	 * At a very high level, this is the logic used to determine the card to
+	 * play:
+	 * <li>If this is the very first turn, and you have the two of clubs, play it
+	 * <li>If someone has already played and you do not have the suit led:
+	 * <ol>If you have the Queen of Spades, play it
+	 * <ol>If you have any hearts, play the highest heart
+	 * <ol>Play the highest card in the suit with the fewest cards in your hand
+	 * <li>If someone has already played and you do have the suit led, figure out
+	 * the highest card that is lower than the current risk threshold.  If there
+	 * are no cards lower than the current risk threshold, play the highest card
+	 * you have in that suit
+	 * <li>If no one has already played:
+	 * <ol>Figure out the suit with the fewest cards (not including hearts)
+	 * <ol>Figure out the highest card in that suit below the current risk
+	 * threshold and play that card
+	 * <ol>If there are no cards in that suit below the current risk threshold,
+	 * check the suit with the second fewest, and then third fewest (again not
+	 * including hearts)
+	 * <ol>If there was still no card below the current risk threshold, determine
+	 * if hearts have already been broken.
+	 * <ol>If hearts have been broken, figure out the highest heart below the
+	 * current risk threshold and play that.
+	 * <ol>If hearts have been broken and there are no hearts below the current
+	 * risk threshold or hearts haven't been broken, pick the highest card in 
+	 * the suit with the fewest cards and play that.
 	 * 
+	 * 
+	 * @param cardsPlayed a List of Cards already played this turn
+	 * @param heartsBroken a boolean indicating if hearts have already been
+	 * broken in this game
+	 * @param veryFirstTurn a boolean indicating if this is the very first
+	 * turn of the game
+	 * @return the Card you are going to play
 	 * @see edu.txstate.hearts.model.Agent#playCard(java.util.List, boolean,
 	 * boolean)
 	 */
@@ -370,11 +453,11 @@ public class AgentAggressive extends Agent {
 				}
 			}
 			/*
-			 * TODO Ensure agent doesn't always default to playing highest card
-			 * when going first. Especially after hearts broken. Particularly in
-			 * this case, the highest card in a suit does not HAVE to be played
-			 * if there is high risk. Might want to consider evaluating all
-			 * suits as well, not necessarily suit with the fewest
+			 * TODO We could use some improved logic around picking the best
+			 * card to play when there are no cards below the current risk
+			 * risk threshold.  Instead of just defaulting to the highest card
+			 * in the smallest suit, perhaps a better option is to find the
+			 * highest card within the risk threshold +10% or something?
 			 */
 			else // no cards played
 			{
@@ -454,6 +537,9 @@ public class AgentAggressive extends Agent {
 				}
 			}
 		}
+		// if we got here, it means it went through everything and didn't
+		// figure out a card to play.  this should never happen, so if it does
+		// throw out some debugging information
 		if(cardToPlay == null)
 		{
 			System.out.println("card is null?");
@@ -484,9 +570,18 @@ public class AgentAggressive extends Agent {
 	}
 
 	/**
-	 * @param cardToPlay
-	 * @param fewestCards
-	 * @return
+	 * This is where the magic happens.  This method will actually iterate
+	 * through the cards being passed in to figure out the highest card with 
+	 * an aggregate risk threshold lower than the current risk threshold.
+	 * The aggregate risk threshold is the probability that someone yet to
+	 * play does not have the suit led and could play hearts times the 
+	 * probability that the current card could win the trick
+	 * @param force a boolean indicating whether it is allowed to return null
+	 * if there are no cards that are under the risk threshold, or if it must
+	 * pick a card to return 
+	 * @param fewestCards the cards to check to find a card to play
+	 * @return the highest card under the risk threshold in the cards passed in,
+	 * if one exists, or else null if force is false
 	 */
 	private Card determineBestCard(List<Card> fewestCards, boolean force) {
 		boolean searching = true;
@@ -569,9 +664,9 @@ public class AgentAggressive extends Agent {
 	 * This method will find the suit with the lowest number of cards, aligning
 	 * with a strategy of removing as many suits as possible.
 	 * 
-	 * @param spades
-	 * @param diamonds
-	 * @param clubs
+	 * @param spades all of the cards in my hand that are spades
+	 * @param diamonds all of the cards in my hand that are diamonds
+	 * @param clubs all of the cards in my hand that are clubs
 	 * @return fewestCards, which is populated with the suit that has the least
 	 *         cards
 	 */
@@ -618,11 +713,16 @@ public class AgentAggressive extends Agent {
 	}
 
 	/**
-	 * Get the probability that a card will win
+	 * Get the probability that a card will win.  If someone has already played
+	 * a card in this suit higher than my card, return 0.  If my card is not the
+	 * suit led, return 0.  Otherwise, this will figure out how 
+	 * many cards are remaining in play in the suit of this card, and how
+	 * many of those cards are less than my card.  Then return
+	 * 1-(cardsSmallerThanMine)/(totalCardsInThatSuit)
 	 * 
-	 * @param test
+	 * @param test the card to test to see if it will win
 	 * @return probability that the passed in card will win all cards played on
-	 *         the table
+	 *         the table as a double between 0 and 1
 	 */
 	private double getProbCardWins(Card test) {
 		List<Integer> availableCards = new ArrayList<Integer>();
@@ -706,7 +806,7 @@ public class AgentAggressive extends Agent {
 	/**
 	 * Obtain all cards within a suit
 	 * 
-	 * @param suit
+	 * @param suit the suit to check
 	 * @return cardsOfSuit, a list of cards that contains every card of a suit
 	 *         in current hand
 	 */
@@ -739,9 +839,11 @@ public class AgentAggressive extends Agent {
 		return false;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
+	/**
+	 * Overrides the baseClass method just to check if I am being passed the
+	 * Queen of Spades, so I can set an internal boolean indicating that I
+	 * have the Queen of Spades
+	 * @param card the card being dealt to the Player
 	 * @see
 	 * edu.txstate.hearts.model.Player#addCard(edu.txstate.hearts.model.Card)
 	 */
@@ -754,6 +856,11 @@ public class AgentAggressive extends Agent {
 		}
 	}
 
+	/**
+	 * The Abstract Agent class forces us to implement this method, but we
+	 * aren't using that at all.  So if it gets called here, throw an exception
+	 * @see edu.txstate.hearts.model.Agent#getCardsToPass()
+	 */
 	@Override
 	public List<Card> getCardsToPass() {
 		// we aren't actually using this, since we actually implemented the real
@@ -762,9 +869,12 @@ public class AgentAggressive extends Agent {
 				"This method shouldn't have gotten called");
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
+	/**
+	 * Overrides the addPlayedCards method from Player to check to see if I
+	 * won the trick, and to adjust my risk thresholds appropriately.
+	 * @param cards the cards in this trick
+	 * @param tookCards a boolean indicating if I won the trick
+	 * @param num the turn number
 	 * @see edu.txstate.hearts.model.Player#addPlayedCards(java.util.Collection,
 	 * boolean)
 	 */
@@ -814,6 +924,8 @@ public class AgentAggressive extends Agent {
 	}
 
 	/**
+	 * Not really used anymore, but here for debugging.  This is used to determine
+	 * how many tricks the Agent expected to win based on the getProbCardWins
 	 * @return the expectedWins
 	 */
 	public double getExpectedWins() {
@@ -821,6 +933,11 @@ public class AgentAggressive extends Agent {
 	}
 
 	/**
+	 * Not really used anymore, but here for debugging.  This is used to determine
+	 * how many actual tricks the Agent won, which can then be compared to the
+	 * expected number of wins to see how well the expected wins is calculated,
+	 * since in the long run, these two numbers should be the same if expected
+	 * wins is being calculated accurately.
 	 * @return the actualWins
 	 */
 	public int getActualWins() {
