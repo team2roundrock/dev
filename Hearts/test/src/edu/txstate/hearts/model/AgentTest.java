a4c89112639934f04e84d0cb5e282f4d7a6ca9f3
Jonathan <js2407@txstate.edu>
2013-04-30 20:31:40 -0500

    Sending Agent test.

---
+++ Hearts/test/src/edu/txstate/hearts/model/AgentTest.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package edu.txstate.hearts.model;
+
+import static org.junit.Assert.*;
+
+import java.util.Iterator;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Jonathan
+ *
+ */
+public class AgentTest {
+	
+	private Agent agent = new AgentGoofy("Test", 0);
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		agent = new AgentGoofy("Test", 0);
+	}
+
+	@Test
+	public void playTwoOfClub() {
+		
+		agent.addCard(new Card(Card.Face.Deuce, Card.Suit.Clubs));
+		Card agentReturn = agent.playTwoOfClub();
+		if((agentReturn.getSuit() != Card.Suit.Clubs) && 
+				agentReturn.getFace() != Card.Face.Deuce)
+		{
+			fail("Card not the deuce of clubs");
+		}
+	}
+}
